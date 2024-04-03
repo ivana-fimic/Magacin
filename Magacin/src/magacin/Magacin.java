d6972a4dbc1303369fd0e4b13df669bb17943b70
Ivana <Ivana@Vivobook>
2024-04-03 17:31:17 +0200

    uradjena implementacija metoda interfejsa

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,24 +1,34 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import magacin.interfejs.MagacinIntefejs;
 
 public class Magacin implements MagacinIntefejs {
-
+    List<Artikal> artikli=new ArrayList<>();
 	@Override
 	public void dodajAtrtikal(Artikal a, int kolicina) {
-		// TODO Auto-generated method stub
+		if(a!=null) {
+			artikli.add(a);
+			a.setKolicina(a.getKolicina()+kolicina);
+		}
+		
 
 	}
 
 	@Override
-	public void obrisiArtikal(int kolicina) {
-		// TODO Auto-generated method stub
+	public void obrisiArtikal(Artikal a,int kolicina) {
+		 a.setKolicina(a.getKolicina()-kolicina);
 
 	}
 
 	@Override
 	public Artikal vratiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		for(Artikal a: artikli) {
+			if(a.getSifra()==sifra)
+				return a;
+		}
 		return null;
 	}
 
--- Magacin/src/magacin/interfejs/MagacinIntefejs.java
+++ Magacin/src/magacin/interfejs/MagacinIntefejs.java
@@ -5,7 +5,7 @@ import magacin.Artikal;
 public interface MagacinIntefejs {
 	public void dodajAtrtikal(Artikal a, int kolicina);
 
-	public void obrisiArtikal(int kolicina);
+	public void obrisiArtikal(Artikal a,int kolicina);
 
 	public Artikal vratiArtikal(int sifra);
 	
