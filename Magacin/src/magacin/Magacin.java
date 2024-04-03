2a46ae6f8341183c60df26093e93c4300d93f50a
Ivana <Ivana@Vivobook>
2024-04-03 17:35:53 +0200

    spajanje

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,12 +1,15 @@
 package magacin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import magacin.interfejs.MagacinIntefejs;
 
 public class Magacin implements MagacinIntefejs {
-
+List<Artikal> artikli=new ArrayList<>();
 	@Override
 	public void dodajAtrtikal(Artikal a, int kolicina) {
-		// TODO Auto-generated method stub
+		artikli.add(a);
 
 	}
 
