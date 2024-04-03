3f64a4ed7df800f611c79d6c7d6fafec4280c1da
Ivana <Ivana@Vivobook>
2024-04-03 17:21:06 +0200

    Napravljena klasa koja implementira intefejs

---
+++ Magacin/src/magacin/Magacin.java
@@ -0,0 +1,25 @@
+package magacin;
+
+import magacin.interfejs.MagacinIntefejs;
+
+public class Magacin implements MagacinIntefejs {
+
+	@Override
+	public void dodajAtrtikal(Artikal a, int kolicina) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiArtikal(int kolicina) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal vratiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
