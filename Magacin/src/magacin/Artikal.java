21058ec259330e035251ca65619ed799e5f1f92f
Ivana <Ivana@Vivobook>
2024-04-03 16:56:00 +0200

    Dodata logicka kontrola

--- Magacin/src/magacin/Artikal.java
+++ Magacin/src/magacin/Artikal.java
@@ -3,55 +3,80 @@ package magacin;
 import java.util.Objects;
 
 public abstract class Artikal {
-private String naziv;
-private int sifra;
-private int kolicina;
-private String opis;
-public String getNaziv() {
-	return naziv;
-}
-public void setNaziv(String naziv) {
-	this.naziv = naziv;
-}
-public int getSifra() {
-	return sifra;
-}
-public void setSifra(int sifra) {
-	this.sifra = sifra;
-}
-public int getKolicina() {
-	return kolicina;
-}
-public void setKolicina(int kolicina) {
-	this.kolicina = kolicina;
-}
-public String getOpis() {
-	return opis;
-}
-public void setOpis(String opis) {
-	this.opis = opis;
-}
-@Override
-public String toString() {
-	return "Artikal [naziv=" + naziv + ", sifra=" + sifra + ", kolicina=" + kolicina + ", opis=" + opis + "]";
-}
-@Override
-public int hashCode() {
-	return Objects.hash(kolicina, naziv, opis, sifra);
-}
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Artikal other = (Artikal) obj;
-	return kolicina == other.kolicina && Objects.equals(naziv, other.naziv) && Objects.equals(opis, other.opis)
-			&& sifra == other.sifra;
-}
+	private String naziv;
+	private int sifra;
+	private int kolicina;
+	private String opis;
 
+	public String getNaziv() {
+		return naziv;
+	}
 
+	public void setNaziv(String naziv) {
+		if (naziv == null)
+			throw new NullPointerException("Naziv ne sme biti null");
+
+		if (naziv.isEmpty()) {
+			throw new IllegalArgumentException("Naziv ne sme biti prazan");
+		}
+		this.naziv = naziv;
+	}
+
+	public int getSifra() {
+		return sifra;
+	}
+
+	public void setSifra(int sifra) {
+		if (sifra < 0)
+			throw new IllegalArgumentException("Sifra ne sme biti manja od nule");
+		this.sifra = sifra;
+	}
+
+	public int getKolicina() {
+		return kolicina;
+	}
+
+	public void setKolicina(int kolicina) {
+		if (kolicina < 0)
+			throw new IllegalArgumentException("Kolicina ne sme biti manja od nule");
+		this.kolicina = kolicina;
+	}
+
+	public String getOpis() {
+		return opis;
+	}
+
+	public void setOpis(String opis) {
+		if (opis == null)
+			throw new NullPointerException("Opis ne sme biti null");
+
+		if (opis.isEmpty()) {
+			throw new IllegalArgumentException("Opis ne sme biti prazan");
+		}
+		this.opis = opis;
+	}
+
+	@Override
+	public String toString() {
+		return "Artikal [naziv=" + naziv + ", sifra=" + sifra + ", kolicina=" + kolicina + ", opis=" + opis + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(kolicina, naziv, opis, sifra);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Artikal other = (Artikal) obj;
+		return kolicina == other.kolicina && Objects.equals(naziv, other.naziv) && Objects.equals(opis, other.opis)
+				&& sifra == other.sifra;
+	}
 
 }
--- Magacin/src/magacin/KucnaHemija.java
+++ Magacin/src/magacin/KucnaHemija.java
@@ -4,41 +4,41 @@ import java.util.Date;
 import java.util.Objects;
 
 public class KucnaHemija extends Artikal {
-private Date rokTrajanja;
+	private Date rokTrajanja;
 
-public Date getRokTrajanja() {
-	return rokTrajanja;
-}
+	public Date getRokTrajanja() {
+		return rokTrajanja;
+	}
 
-public void setRokTrajanja(Date rokTrajanja) {
-	this.rokTrajanja = rokTrajanja;
-}
+	public void setRokTrajanja(Date rokTrajanja) {
+		if (rokTrajanja.before(new Date()))
+			throw new IllegalArgumentException("Datum ne sme biti u proslosti");
+		this.rokTrajanja = rokTrajanja;
+	}
 
-@Override
-public String toString() {
-	return "KucnaHemija [rokTrajanja=" + rokTrajanja + "]";
-}
+	@Override
+	public String toString() {
+		return "KucnaHemija [rokTrajanja=" + rokTrajanja + "]";
+	}
 
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = super.hashCode();
-	result = prime * result + Objects.hash(rokTrajanja);
-	return result;
-}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(rokTrajanja);
+		return result;
+	}
 
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (!super.equals(obj))
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	KucnaHemija other = (KucnaHemija) obj;
-	return Objects.equals(rokTrajanja, other.rokTrajanja);
-}
-
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		KucnaHemija other = (KucnaHemija) obj;
+		return Objects.equals(rokTrajanja, other.rokTrajanja);
+	}
 
 }
